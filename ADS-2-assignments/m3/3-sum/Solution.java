319e09fae9035ca20ae132d7286b00b062b36c07
KujithaMannepuri <kujitha120@msitprogram.net>
2018-09-26 23:09:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-2-assignments/m3/3-sum/Solution.java
+++ ADS-2-assignments/m3/3-sum/Solution.java
@@ -5,90 +5,96 @@ import java.util.Arrays;
  * Class for three sum.
  */
 class ThreeSum {
-	/**
-	 * array.
-	 */
-	private int[] arr;
-	/**
-	 * array size.
-	 */
-	private int size;
+    /**
+     * array.
+     */
+    private int[] arr;
+    /**
+     * array size.
+     */
+    private int size;
 
-	/**
-	 * constructor.
-	 *
-	 * @param      arr2   The arr 2
-	 * @param      size1  The size 1
-	 */
-	ThreeSum(final int[] arr2, final int size1) {
-		this.arr = arr2;
-		this.size = size1;
-	}
-	
-	/**
-	 * binary search function.
-	 *
-	 * @param      arr   The arr
-	 * @param      val   The value
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int BinarySearch(final int[] arr, final int val) {
-		int min = 0;
-		int max = arr.length - 1;
-		while (min <= max) {
-			int mid = (min + max)/2;
-			if (arr[mid] == val) {
-				return mid;
-			} else if (arr[mid] < val) {
-				min = mid + 1;
-			} else {
-				max = mid - 1;
-			}
-		}
-		return -1;
-	}
+    /**
+     * constructor.
+     *
+     * @param      arr2   The arr 2
+     * @param      size1  The size 1
+     */
+    ThreeSum(final int[] arr2, final int size1) {
+        this.arr = arr2;
+        this.size = size1;
+    }
+    
+    /**
+     * binary search function.
+     *
+     * @param      arr   The arr
+     * @param      val   The value
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int binarySearch(final int[] arr1, final int val) {
+        int min = 0;
+        int max = arr1.length - 1;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (arr1[mid] == val) {
+                return mid;
+            } else if (arr1[mid] < val) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return -1;
+    }
 
-	/**
-	 * total sum function.
-	 *
-	 * @param      arr   The arr
-	 *
-	 * @return     returns count of numbers.
-	 */
-	public int totalsum(final int[] arr) {
-		Arrays.sort(arr);
-		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				int tot = BinarySearch(arr, -(arr[i] + arr[j]));
-				if (tot > j && tot > 0) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
+    /**
+     * total sum function.
+     *
+     * @param      arr2   The arr
+     *
+     * @return     returns count of numbers.
+     */
+    public int totalsum(final int[] arr2) {
+        Arrays.sort(arr2);
+        int count = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = i + 1; j < arr2.length; j++) {
+                int tot = binarySearch(arr2, -(arr2[i] + arr2[j]));
+                if (tot > j && tot > 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
 
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		//int num = Integer.parseInt(scan.nextLine());
-		int num = scan.nextInt();
-		int[] arr = new int[num];
-		ThreeSum t_sum = new ThreeSum(arr, num);
-		for (int i = 0; i < num; i++) {
-			arr[i] = scan.nextInt();			
-		}
-		System.out.println(t_sum.totalsum(arr));
-	}
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // constructor not used.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //int num = Integer.parseInt(scan.nextLine());
+        int num = scan.nextInt();
+        int[] arr = new int[num];
+        ThreeSum tsum = new ThreeSum(arr, num);
+        for (int i = 0; i < num; i++) {
+            arr[i] = scan.nextInt();         
+        }
+        System.out.println(tsum.totalsum(arr));
+    }
 }
