9402ccb96ed92d9aca198e2cc9cec7089f9853b8
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-01 18:21:19 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1-assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-assignments/m7/7.1 Steque/Solution.java
@@ -1,47 +1,52 @@
 import java.util.Scanner;
-
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * main function.
-	 *
-	 * @param args  The arguments
-	 */
-	public static void main(final String[] args) {
-	Scanner sc = new Scanner(System.in);
-	Steque ste = new Steque();
-	String num = sc.nextLine();
-	while (sc.hasNext()) {
-		String[] token = sc.nextLine().split(" ");
-		// if(token.length==1 && !token[0].equals("pop")){
-		// 	ste = new Steque();
-		// 	System.out.println();
-		// 	continue;
-		// }
-		switch (token[0]) {
-			case "push":
-				ste.push(token[1]);
-				ste.printStack();
-			break;
-			case "pop":
-			try {
-			ste.pop();
-			ste.printStack();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-			break;
-			case "enqueue":
-			ste.enqueue(token[1]);
-			ste.printStack();
-			break;
-			default:
-			System.out.println();
-			ste = new Steque();
-			break;
-		}
-	}
+    /**
+     * constructor.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function.
+     *
+     * @param args  The arguments
+     */
+    public static void main(final String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Steque ste = new Steque();
+    String num = sc.nextLine();
+    while (sc.hasNext()) {
+        String[] token = sc.nextLine().split(" ");
+        // if(token.length==1 && !token[0].equals("pop")){
+        //  ste = new Steque();
+        //  System.out.println();
+        //  continue;
+        // }
+        switch (token[0]) {
+            case "push":
+                ste.push(token[1]);
+                ste.printStack();
+            break;
+            case "pop":
+            try {
+            ste.pop();
+            ste.printStack();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            break;
+            case "enqueue":
+            ste.enqueue(token[1]);
+            ste.printStack();
+            break;
+            default:
+            System.out.println();
+            ste = new Steque();
+            break;
+        }
+    }
 }
 }
