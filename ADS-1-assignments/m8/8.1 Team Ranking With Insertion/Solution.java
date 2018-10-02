0ba410a0bc837f7d24c45f624cd8223d19ecbd66
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-02 21:28:10 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		SelectionSort ip = new SelectionSort();
-		while (sc.hasNext()) {
-			String[] token = sc.nextLine().split(",");
-			Team games = new Team(token[0], Integer.parseInt(token[1]),
-				Integer.parseInt(token[2]), Integer.parseInt(token[3]));
-			ip.add(games);
-		}
-		ip.sorting();
-		ip.tostring();
-	}
+class Solution {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SelectionSort ip = new SelectionSort();
+        while (sc.hasNext()) {
+            String[] token = sc.nextLine().split(",");
+Team games = new Team(token[0], Integer.parseInt(token[1]),
+    Integer.parseInt(token[2]), Integer.parseInt(token[2 + 1]));
+            ip.add(games);
+        }
+        ip.sorting();
+        ip.tostring();
+    }
 }
