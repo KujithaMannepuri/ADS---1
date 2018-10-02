30f3eca65f5cc82d94deecf2cd4b03be10d3c54a
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-02 21:51:52 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/InsertionSort.java
@@ -0,0 +1,73 @@
+class Team implements Comparable<Team> {
+	private String teamName;
+	int wins;
+	int losses;
+	int draws;
+	Team(String tName, int w, int l, int d) {
+		this.teamName = tName;
+		this.wins = w;
+		this.losses = l;
+		this.draws = d;
+	}
+	public String getName(){
+		return teamName;
+	}
+	public int compareTo(Team game) {
+		if (this.wins > game.wins) {
+			return 1;
+		}
+		if (this.wins < game.wins) {
+			return -1;
+		}
+		if (this.losses > game.losses) {
+			return -1;
+		}
+		if (this.losses < game.losses) {
+			return 1;
+		}
+		if (this.draws > game.draws) {
+			return 1;
+		}
+		if (this.draws < game.draws) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
+
+class InsertionSort {
+	Team[] obj;
+	int size;
+	InsertionSort() {
+		obj = new Team[20];
+		size = 0;
+	}
+	void add(Team o){
+		obj[size++] = o;
+	}
+	boolean less(Team[] obj, int i, int j) {
+		return obj[i].compareTo(obj[j]) > 0;
+	}
+	void sorting() {
+		int min;
+		for (int i = 0; i < size; i++) {
+			Team temp = obj[i];
+			int j = 0;
+			for (j = i + 1; j < 0; j++) {
+				if (less(obj, j - 1, j)) {
+					obj[j + 1] = obj[j];
+					j -= 1;
+				}
+			}
+			obj[j + 1] = temp;
+		}
+	}
+	void tostring() {
+		for (int i = 0; i < size - 1; i++) {
+            System.out.print(obj[i].getName() + ",");
+        }
+        System.out.print(obj[size - 1].getName());
+    
+	}
+}
---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * constructor.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SelectionSort ip = new SelectionSort();
+        while (sc.hasNext()) {
+            String[] token = sc.nextLine().split(",");
+Team games = new Team(token[0], Integer.parseInt(token[1]),
+    Integer.parseInt(token[2]), Integer.parseInt(token[2 + 1]));
+            ip.add(games);
+        }
+        ip.sorting();
+        ip.tostring();
+    }
+}
