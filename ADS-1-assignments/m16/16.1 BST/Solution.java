f14e8a9b40e846e7b1a66faec52a030538127c22
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-12 10:03:36 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m16/16.1 BST/Solution.java
+++ ADS-1-assignments/m16/16.1 BST/Solution.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
+class Node {
+	Book data;
+	int val;
+	Node left;
+	Node right;
+	Node(Book k, int v) {
+		this.data = k;
+		this.val = v;
+	}
+}
+
 class Book {
 	String author;
 	String name;
@@ -10,26 +23,17 @@ class Book {
 		this.author = a;
 		this.price = p;
 	}
-	String getauthor() {
-		return this.author;
-	}
 	String getname() {
 		return this.name;
+	}
+	String getauthor() {
+		return this.author;
 	}
 	Float getprice() {
 		return this.price;
 	}
 }
-class Node {
-	Book data;
-	int val;
-	Node n1;
-	Node n2;
-	Node(Book k, int v) {
-		this.data = (k);
-		this.val = v;
-	}
-}
+
 class BinarySearchTree<Key extends Comparable<Key>, Value> {
 	Node root;
 	Book book;
@@ -46,10 +50,10 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 		int cmp = key.getname().compareTo(x.data.getname());
 		if (cmp < 0) {
-			x.n1 = put(x.n1, key, val);
+			x.left = put(x.left, key, val);
 		}
 		else if (cmp > 0) {
-			x.n2 = put(x.n2, key, val);
+			x.right = put(x.right, key, val);
 		}
 		else if (cmp == 0) {
 			x.val = val;
@@ -61,15 +65,38 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		while (x != null) {
 			int cmp = key.getname().compareTo(x.data.getname());
 			if (cmp < 0) {
-				x = x.n1;
+				x = x.left;
 			}
 	        if (cmp > 0) {
-	        	x = x.n2;
+	        	x = x.right;
 	        }
 	        if (cmp == 0) {
 	        	return x.val;
 	        } 	
 		}
 		return null;
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BinarySearchTree bst = new BinarySearchTree();
+		while (sc.hasNext()) {
+			String[] token = sc.nextLine().split(",");
+			switch(token[0]) {
+				case "get":
+				Book ip1 = new Book(token[1], token[2], Float.parseFloat(token[3]));
+				Integer i = bst.get(ip1);
+				System.out.println(i);
+				break;
+				case "put":
+				Book ip2 = new Book(token[1], token[2], Float.parseFloat(token[3]));
+				bst.put(ip2, Integer.parseInt(token[4]));
+				break;
+				default:
+				break;
+			}
+		}
 	}
 }
