734a3db2dd6d547ccbaf017be7fbaeb81644024e
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-27 13:00:24 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m24/1/Queue.java
@@ -0,0 +1,432 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+public class Queue<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    /**
+     * Initializes an empty queue.
+     */
+    public Queue() {
+        first = null;
+        last  = null;
+        n = 0;
+    }
+
+    /**
+     * Returns true if this queue is empty.
+     *
+     * @return {@code true} if this queue is empty; {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this queue.
+     *
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns the item least recently added to this queue.
+     *
+     * @return the item least recently added to this queue
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    /**
+     * Adds the item to this queue.
+     *
+     * @param  item the item to add
+     */
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        n++;
+    }
+
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     *
+     * @return the item on this queue that was least recently added
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        return item;
+    }
+
+    /**
+     * Returns a string representation of this queue.
+     *
+     * @return the sequence of items in FIFO order, separated by spaces
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    } 
+
+    /**
+     * Returns an iterator that iterates over the items in this queue in FIFO order.
+     *
+     * @return an iterator that iterates over the items in this queue in FIFO order
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+}
+class SequentialSearchST<Key, Value> {
+    private int size;
+    /**
+     * { var_description }.
+     */
+    private Node first;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * { var_description }.
+         */
+        private Key key;
+        /**
+         * { var_description }.
+         */
+        private Value val;
+        /**
+         * { var_description }.
+         */
+        private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      k     { parameter_description }
+         * @param      v     { parameter_description }
+         * @param      n     { parameter_description }
+         */
+        Node(final Key k, final Value v, final Node n)  {
+            this.key  = k;
+            this.val  = v;
+            this.next = n;
+        }
+    }
+    /**
+     * Initializes an empty symbol table.
+     */
+    SequentialSearchST() {
+    }
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
+    public int getSize() {
+        return size;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final Key key) {
+        return get(key) != null;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Value get(final Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.val;
+            }
+        }
+        return null;
+    }
+    /**
+     * put function.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final Key key, final Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        size++;
+    }
+    /**
+     * delete function.
+     *
+     * @param      key   The key
+     */
+    public void delete(final Key key) {
+        first = delete(first, key);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node delete(final Node x, final Key key) {
+        if (x == null) {
+            return null;
+        }
+        if (key.equals(x.key)) {
+            size--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Key> keys()  {
+        Queue<Key> queue = new Queue<Key>();
+        for (Node x = first; x != null; x = x.next) {
+            queue.enqueue(x.key);
+        }
+        return queue;
+    }
+}
+
+class SeparateChainingHashST<Key, Value> {
+    /**
+     * variable declaration.
+     */
+    private static final int INIT_CAPACITY = 4;
+    /**
+     * number of key-value pairs.
+     */
+    private int n;
+    /**
+     * hash table size.
+     */
+    private int m;
+    /**
+     * { var_description }.
+     */
+    private SequentialSearchST<Key, Value>[] st;
+    /**
+     * Initializes an empty symbol table.
+     */
+    SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    }
+    /**
+     * constructor.
+     *
+     * @param      m1    The m 1
+     */
+    SeparateChainingHashST(final int m1) {
+        this.m = m1;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++) {
+            st[i] = new SequentialSearchST<Key, Value>();
+        }
+    }
+    /**
+     * resize function.
+     *
+     * @param      chains  The chains
+     */
+    private void resize(final int chains) {
+        SeparateChainingHashST<Key, Value> temp = new
+        SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m  = temp.m;
+        this.n  = temp.n;
+        this.st = temp.st;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int hash(final Key key) {
+        final int num = 0x7fffffff;
+        return (key.hashCode() & num) % m;
+    }
+    /**
+     * size.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int size() {
+        return n;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+            "argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+    /**
+     * get function.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Value get(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key is null");
+        }
+        int i = hash(key);
+        return st[i].get(key);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final Key key, final Value val) {
+        /**
+         * { var_description }.
+         */
+        final int ten = 10;
+        if (key == null) {
+            throw new IllegalArgumentException(
+            "first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        if (n >= ten * m) {
+            resize(2 * m);
+        }
+        int i = hash(key);
+        if (!st[i].contains(key)) {
+            n++;
+        }
+        st[i].put(key, val);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      key   The key
+     */
+    public void delete(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+            "argument to delete() is null");
+        }
+        int i = hash(key);
+        if (st[i].contains(key)) {
+            n--;
+        }
+        st[i].delete(key);
+        if (m > INIT_CAPACITY && n <= 2 * m) {
+            resize(m / 2);
+    }
+}
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                queue.enqueue(key);
+            }
+        }
+        return queue;
+    }
+}
---
+++ ADS-1-assignments/m24/1/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class StudentInfo {
+	int rollNum;
+	String name;
+	Double marks;
+	StudentInfo(int num, String stdnt, Double m) {
+		this.rollNum = num;
+		this.name = stdnt;
+		this.marks = m;
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        SeparateChainingHashST<Integer, String> hashSt1 = new
+                    SeparateChainingHashST<Integer, String>();
+        SeparateChainingHashST<Integer, Double> hashSt2 = new
+                    SeparateChainingHashST<Integer, Double>();
+        int n1 = sc.nextInt();
+        for (int i = 0; i < n1; i++) {
+        	String[] token1 = sc.nextLine().split(",");
+			hashSt1.put(Integer.parseInt(token1[0]), token1[1]);
+			hashSt2.put(Integer.parseInt(token1[0]), Double.parseDouble(token1[2]));
+		}
+		int n2 = sc.nextInt();
+        for (int i = 0; i < n2; i++) {
+        	String[] token2 = sc.nextLine().split(" ");
+				if (token2[2].equals("1")){
+					if (hashSt1.get(Integer.parseInt(token2[1])) == null) {
+						System.out.println("Student doesn't exists...");
+					}
+					else {
+						System.out.println(hashSt1.get(Integer.parseInt(token2[1])));
+					}
+				} else if (token2[2].equals("2")){
+					if (hashSt2.get(Integer.parseInt(token2[1])) == null) {
+						System.out.println("Student doesn't exists...");
+					}
+					else {
+						System.out.println(hashSt2.get(Integer.parseInt(token2[2])));
+					}
+		}
+	}
+}
+}
